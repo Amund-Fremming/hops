666f2bfe421c72d63dea01e6e169bb40d3bf4172
amund-fremming <amund.fremming@gmail.com>
2026-07-07 16:43:16 +0200

    feat: restructure authentication routes and remove unused test endpoint

--- src/presentation/auth.rs
+++ src/presentation/auth.rs
@@ -7,7 +7,6 @@ use crate::domain::{error::ServerError, state::AppState, user::LoginRequest};
 
 pub fn auth_routes(state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/authenticate", post(authenticate))
         .route("/", post(authenticate))
         .with_state(state)
 }
